749346347f32a9d751725b237e59ee05ef1060d5
DCisero <deshaylacisero@gmail.com>
2017-08-01 16:08:37 -0400

    DeShayla updated app with for loop to print numbered list of items

--- src/MenuApp.java
+++ src/MenuApp.java
@@ -4,7 +4,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class MenuApp{
+public class MenuApp {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -14,23 +14,42 @@ public class MenuApp{
 
         System.out.println("Menu Options");
         System.out.println("........................");
-        
+
         Inventory inventory = new Inventory();
-        //inventory.readFile("./Inventory.txt");
 
         ArrayList<Product> inventoryList = inventory.readFile("./Inventory.txt");
         if (inventoryList == null) {
             System.out.println("Issue with file... closing");
             return;
         }
+        for (int i = 1; i < inventoryList.size(); i++) {
+            if (inventoryList.get(i) instanceof Product) {
+                Product p = (Product) (inventoryList.get(i));
+                {
 
-        //output this list
-        //enhanced for (foreach) to go through list
-        for (Product c : inventoryList) {
-            //toString does the formatting
-            System.out.println(c);
+                    //output this list
+                    //enhanced for (foreach) to go through list
+                    for (Product c : inventoryList) {
+                        //toString does the formatting
+                        System.out.println(i + ":" + c);
+                        i++;
+                    }
+
+                    System.out.println();
+
+                    int menuInput;
+
+                    System.out.print("Please select a menu item: ");
+                    menuInput = scan.nextInt();
+
+                    if (menuInput == 1) {
+                        System.out.println(inventoryList.get(0));
+                    }
+
+
+                }
+
+            }
         }
-
     }
-
 }
